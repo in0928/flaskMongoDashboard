47e548521874cd9fd90e7a248091652631c91a68
in0928 <19920928+in0928@users.noreply.github.com>
2019-07-29 00:04:11 +0530

    Try Again

---
+++ app.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for  # For flask implementation
+from bson import ObjectId  # For ObjectId to work
+from pymongo import MongoClient
+
+app = Flask(__name__)
+title = "Sample form submission with MongoDB"
+heading = "Form submission with Flask and MongoDB"
+
+client = MongoClient("mongodb://127.0.0.1:27017")  # host uri
+db = client.test  # Select the database
+people_test = db.peopleTest  # Select the collection name
+tracker = []
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/people")
+def people():
+    return render_template("people.html")
+
+
+@app.route("/action", methods=["POST"])
+def action():
+    # get data
+    full_name = request.form.get("full-name")
+    age = request.form.get("age")
+    gender = request.form.get("gender")
+    status = request.form.get("status")
+    description = request.form.get("description")
+    people_test.insert_one({
+        "full-name": full_name,
+        "age": age,
+        "gender": gender,
+        "status": status,
+        "description": description,
+        "last-updated": datetime.now()
+    })
+    return redirect("/submitted")
+
+
+@app.route("/submitted")
+def submitted():
+    return  render_template("submitted.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

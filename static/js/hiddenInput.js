75b08948e456bcec31f1886bbb17d55ec3508d82
in0928asu <49297560+koasurion@users.noreply.github.com>
2019-08-07 17:16:46 +0530

    Form fixed

---
+++ static/js/hiddenInput.js
@@ -0,0 +1,7 @@
+function otherCheck(that) {
+    if (that.value == "others") {
+        document.getElementById("input-status").style.display = "flex";
+    } else {
+        document.getElementById("input-status").style.display = "none";
+    }
+}

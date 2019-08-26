0b2f4aaba60988e74d8518f4d7a7411ac57f7024
in0928asu <49297560+koasurion@users.noreply.github.com>
2019-08-26 18:36:44 +0900

    people now can be updated through update form

--- app.py
+++ app.py
@@ -9,19 +9,17 @@ from mongoDBConnector import PyMongo
 from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__)
-title = "Sample form submission with MongoDB"
-heading = "Form submission with Flask and MongoDB"
 
 connector = PyMongo()
-people_test = connector.people_test
-schedule_test = connector.schedule_test
-union_test = connector.union_test
-ba_test = connector.ba_test
-my_dreams_test = connector.my_dreams_test
-place_test = connector.place_test
-my_dlines_test = connector.my_dlines_test
-users_test = connector.users_test
-notes_test = connector.notes_test
+people_data = connector.people
+schedule_this_month_data = connector.schedule_this_month
+unions_data = connector.unions
+ba_data = connector.ba
+my_dreams_data = connector.my_dreams
+places_data = connector.places
+my_dlines_data = connector.my_dlines
+users_data = connector.users
+notes_data = connector.notes
 
 @app.route("/confirm")
 def favorite():
@@ -31,11 +29,11 @@ def favorite():
 @app.route("/")
 def home():
     t = "My Dashboard"
-    people_list = people_test.find()
-    ba_list = ba_test.find()
-    fav_people_list = people_test.find({"favorite": "True"})
-    fav_ba_list = ba_test.find({"favorite": "True"})
-    fav_notes_list = notes_test.find({"favorite": "True"})
+    people_list = people_data.find()
+    ba_list = ba_data.find()
+    fav_people_list = people_data.find({"favorite": True})
+    fav_ba_list = ba_data.find({"favorite": True})
+    fav_notes_list = notes_data.find({"favorite": True})
     return render_template("home.html", people_list=people_list, ba_list=ba_list,\
                            fav_people_list=fav_people_list, fav_ba_list=fav_ba_list, fav_notes_list=fav_notes_list, t=t)
 
@@ -45,20 +43,20 @@ def form():
     return render_template("form.html")
 
 @app.route("/updatepeopleform")
-def update_form():
+def form_u():
     id = request.values.get("_id")
-    person = people_test.find({"_id": ObjectId(id)})
-    print(person)
+    print(id)
+    person = people_data.find({"_id": ObjectId(id)})
     return render_template("updateForm.html", person=person)
 
-@app.route("/submit_form", methods=["POST"])
-def submit_form():
-    # get data from form
-    target_list = request.form.get("submit-to-list")
+@app.route("/updatedpeople", methods=['POST'])
+def update_form():
+    id = str(request.values.get("_id"))
     full_name = request.form.get("full-name")
     age = request.form.get("age")
     gender = request.form.get("gender")
     status = request.form.get("status")
+    other_status = request.form.get("other-status")
     description = request.form.get("description")
     needs = request.form.get("needs")
     character = request.form.get("character")
@@ -70,15 +68,73 @@ def submit_form():
     hometown = request.form.get("hometown")
     source = request.form.get("source")
     remarks = request.form.get("remarks")
-    if target_list == "people-list":
-        cursor = people_test
+    fav = request.form.get("fav")
+    if fav:
+        favorite = True
     else:
-        cursor = ba_test
+        favorite = False
+    people_data.find_one_and_update({"_id": ObjectId(id)},
+                                    {'$set': {
+                                        "favorite": favorite,
+                                        "full-name": full_name,
+                                        "age": age,
+                                        "gender": gender,
+                                        "status": status,
+                                        "other-status": other_status,
+                                        "description": description,
+                                        "needs": needs,
+                                        "character": character,
+                                        "relation": relation,
+                                        "job": job,
+                                        "company": company,
+                                        "company-loc": company_loc,
+                                        "home": home,
+                                        "hometown": hometown,
+                                        "source": source,
+                                        "remarks": remarks,
+                                        "last-updated": datetime.now()
+                                    }})
+
+    return redirect("/submitted")
+
+
+@app.route("/submit_form", methods=["POST"])
+def submit_form():
+    # get data from form
+    target_list = request.form.get("submit-to-list")
+    full_name = request.form.get("full-name")
+    age = request.form.get("age")
+    gender = request.form.get("gender")
+    status = request.form.get("status")
+    other_status = request.form.get("other-status")
+    description = request.form.get("description")
+    needs = request.form.get("needs")
+    character = request.form.get("character")
+    relation = request.form.get("relation")
+    job = request.form.get("job")
+    company = request.form.get("company")
+    company_loc = request.form.get("company-loc")
+    home = request.form.get("home")
+    hometown = request.form.get("hometown")
+    source = request.form.get("source")
+    remarks = request.form.get("remarks")
+    fav = request.form.get("fav")
+    if fav:
+        favorite = True
+    else:
+        favorite = False
+
+    if target_list == "ba-list":
+        cursor = ba_data
+    else:
+        cursor = people_data
     cursor.insert_one({
+        "favorite": favorite,
         "full-name": full_name,
         "age": age,
         "gender": gender,
         "status": status,
+        "other-status": other_status,
         "description": description,
         "needs": needs,
         "character": character,
@@ -110,7 +166,7 @@ def schedule():
 
     event_list = []
     for day in month_date:
-        aug_events = schedule_test.find({"date": day})
+        aug_events = schedule_this_month_data.find({"date": day})
         merged = ds.merge_rows(aug_events)
 
         for i in merged:
@@ -124,13 +180,13 @@ def schedule():
 @app.route("/people")
 def people():
     t = "People List"
-    people_list = people_test.find()
+    people_list = people_data.find()
     return render_template("people.html", people_list=people_list, t=t)
 
 @app.route("/ba")
 def ba():
     t = "BA List"
-    ba_list = ba_test.find()
+    ba_list = ba_data.find()
     return render_template("ba.html", ba_list=ba_list, t=t)
 
 
--- mongoDBConnector.py
+++ mongoDBConnector.py
@@ -4,14 +4,29 @@ from pymongo import MongoClient
 class PyMongo:
 
     def __init__(self):
-        self.client = MongoClient("mongodb://127.0.0.1:27017")  # host uri
-        self.db = self.client.test  # Select the database
-        self.people_test = self.db.peopleTest  # 0
-        self.schedule_test = self.db.scheduleTest  # 1
-        self.union_test = self.db.unionTest  # 2 this is used with schedule
-        self.ba_test = self.db.baTest  # 3
-        self.my_dreams_test = self.db.myDreamsTest  # 4
-        self.place_test = self.db.placeTest  # 5
-        self.my_dlines_test = self.db.myDlinesTest  # 7 TODO: add d-lines management someday
-        self.users_test = self.db.usersTest  # 7 TODO: add user management someday
-        self.notes_test = self.db.notesTest  # 8
+        # # tests
+        # self.client = MongoClient("mongodb://127.0.0.1:27017")  # host uri
+        # self.db = self.client.test  # Select the database
+        # self.people_test = self.db.peopleTest  # 0
+        # self.schedule_test = self.db.scheduleTest  # 1
+        # self.union_test = self.db.unionTest  # 2 this is used with schedule
+        # self.ba_test = self.db.baTest  # 3
+        # self.my_dreams_test = self.db.myDreamsTest  # 4
+        # self.place_test = self.db.placeTest  # 5
+        # self.my_dlines_test = self.db.myDlinesTest  # 7 TODO: add d-lines management someday
+        # self.users_test = self.db.usersTest  # 7 TODO: add user management someday
+        # self.notes_test = self.db.notesTest  # 8
+
+        # real DB
+        self.client = MongoClient("mongodb+srv://in0928:trybest920928LAISAI@cluster0-nfgyd.gcp.mongodb.net/test?retryWrites=true&w=majority")  # host uri
+        self.db = self.client.NSDB  # Select the database
+        self.people = self.db.people  # 0
+        self.schedule_this_month = self.db.scheduleThisMonth  # 1
+        self.schedule_next_month = self.db.scheduleNextMonth
+        self.unions = self.db.unions  # 2 this is used with schedule
+        self.ba = self.db.ba  # 3
+        self.my_dreams = self.db.myDreams  # 4
+        self.places = self.db.places  # 5
+        self.my_dlines = self.db.myDlines  # 7 TODO: add d-lines management someday, can be premium feature
+        self.users = self.db.users  # 7 TODO: add user management someday
+        self.notes = self.db.notes  # 8
